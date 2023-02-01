f4abf1555ee94eeb05e0f87508744e8ede9fffb5
Manuel Takata <takata211011@gmail.com>
2023-02-01 00:23:46 -0500

    add router to app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,44 @@
 import React from 'react'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import Products from './pages/Products/Products'
+import Product from './pages/Product/Product'
+import Navbar from './components/Navbar/Navbar'
+import Footer from './components/Footer/Footer'
 export default function App() {
-  return <div>App</div>
+  const Layout = () => {
+    return (
+      <div className="app">
+        <Navbar />
+        <Outlet />
+        <Footer />
+      </div>
+    )
+  }
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Layout />,
+      children: [
+        {
+          path: '/',
+          element: <Home />,
+        },
+        {
+          path: '/products/:id',
+          element: <Products />,
+        },
+        {
+          path: '/product/:id',
+          element: <Product />,
+        },
+      ],
+    },
+  ])
+
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  )
 }
