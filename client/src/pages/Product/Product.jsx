3b4d896d4a9c29179ae04d338cda576c2426f22e
Manuel Takata <takata211011@gmail.com>
2023-02-01 22:57:01 -0500

    add products and product page

--- client/src/pages/Product/Product.jsx
+++ client/src/pages/Product/Product.jsx
@@ -1,7 +1,89 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import {
+  MdAddShoppingCart,
+  MdFavoriteBorder,
+  MdAccountBalanceWallet,
+} from 'react-icons/md'
 export default function Product() {
+  const [selectedImg, setSelectedImg] = useState(0)
+  const [quantity, setQuantity] = useState(0)
+  const images = [
+    'https://images.pexels.com/photos/1018911/pexels-photo-1018911.jpeg?auto=compress&cs=tinysrgb&w=600',
+    'https://images.pexels.com/photos/1507698/pexels-photo-1507698.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load',
+  ]
   return (
-    <div>Product</div>
+    <main className="flex gap-12 px-12 py-5 -product">
+      <section className="flex flex-1 gap-5 -left">
+        <div className="flex-1 -images">
+          <img
+            src={images[0]}
+            alt=""
+            onClick={(e) => setSelectedImg(0)}
+            className="object-cover w-full cursor-pointer h-36"
+          />
+          <img
+            src={images[1]}
+            alt=""
+            onClick={(e) => setSelectedImg(1)}
+            className="object-cover w-full cursor-pointer h-36"
+          />
+        </div>
+        <div className="-mainImages flex-[5]">
+          <img
+            src={images[selectedImg]}
+            alt=""
+            className="w-full max-h-[800px] object-cover"
+          />
+        </div>
+      </section>
+      <section className="flex flex-col flex-1 gap-8 -right">
+        <h1>Title</h1>
+        <span className="text-3xl text-[#2879fe] font-medium">$199</span>
+        <p className="text-lg font-light text-justify">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus
+          iusto iste hic dolore omnis exercitationem. Saepe delectus ex qui.
+          Fugit.
+        </p>
+        <div className="flex items-center gap-2 -quantity">
+          <button
+            onClick={() => setQuantity((prev) => (prev === 1 ? 1 : prev - 1))}
+            className="flex items-center justify-center w-12 h-12 bg-gray-200 border-none cursor-pointer"
+          >
+            -
+          </button>
+          {quantity}
+          <button
+            onClick={() => setQuantity((prev) => prev + 1)}
+            className="flex items-center justify-center w-12 h-12 bg-gray-200 border-none cursor-pointer"
+          >
+            +
+          </button>
+        </div>
+        <button className="-add w-[250px] p-3 bg-[#2879fe] text-white flex items-center justify-center gap-5 cursor-pointer border-none font-medium">
+          <MdAddShoppingCart /> ADD TO CART
+        </button>
+        <div className="flex gap-5 -links">
+          <button className="-item flex items-center gap-2 text-[14px] text-[#2879fe]">
+            <MdFavoriteBorder /> ADD TO WISH LIST
+          </button>
+          <button className="-item flex items-center gap-2 text-[14px] text-[#2879fe]">
+            <MdAccountBalanceWallet /> ADD TO COMPARE
+          </button>
+        </div>
+        <div className="-info flex flex-col gap-3 text-gray-500 text-[14px] mt-7">
+          <span>Vendor: Polo</span>
+          <span>Product Type: T-Shirt</span>
+          <span>Tag: T-Shirt, Women, Top</span>
+        </div>
+        <hr className="border border-solid border-gray-200" />
+        <div className="info flex flex-col gap-3 text-gray-500 text-[14px] mt-7">
+          <span>DESCRIPTION</span>
+          <hr className="w-[200px] border border-solid border-gray-200" />
+          <span>ADDITIONAL INFORMATION</span>
+          <hr className="w-[200px] border border-solid border-gray-200" />
+          <span>FAQ</span>
+        </div>
+      </section>
+    </main>
   )
 }
--- client/src/pages/Products/Products.jsx
+++ client/src/pages/Products/Products.jsx
@@ -1,5 +1,89 @@
-import React from 'react'
+import { useState } from 'react'
+import { useParams } from 'react-router-dom'
+import List from '../../components/List/List'
 
 export default function Products() {
-  return <div>Products</div>
+  const catId = parseInt(useParams().id)
+  const [maxPrice, setMaxPrice] = useState(1000)
+  const [sort, setSort] = useState(null)
+
+  return (
+    <main className="flex px-12 py-8 -products">
+      <aside className="flex-1 -left sticky h-full top-12">
+        <div className="-filterItem mb-8">
+          <h2 className="font-normal mb-5">Product Categories</h2>
+          <div className="-input-item mb-3">
+            <input type="checkbox" name="1" id="1" value={1} />
+            <label className="ml-3" htmlFor="1">
+              Shoes
+            </label>
+          </div>
+          <div className="-input-item mb-3">
+            <input type="checkbox" name="2" id="2" value={2} />
+            <label className="ml-3" htmlFor="2">
+              Skirt
+            </label>
+          </div>
+          <div className="-input-item mb-3">
+            <input type="checkbox" name="3" id="3" value={3} />
+            <label className="ml-3" htmlFor="3">
+              Coast
+            </label>
+          </div>
+        </div>
+        <div className="-filterItem mb-8">
+          <h2 className="font-normal mb-5">Filter by Price</h2>
+          <div className="-input-item mb-3">
+            <span>0</span>
+            <input
+              type="range"
+              name=""
+              id=""
+              min={0}
+              max={100}
+              onChange={(e) => setMaxPrice(e.target.value)}
+            />
+            <span>{maxPrice}</span>
+          </div>
+        </div>
+        <div className="-filterItem mb-8">
+          <h2 className="font-normal mb-5">Sort by</h2>
+          <div className="-input-item mb-3">
+            <input
+              type="radio"
+              name="asc"
+              id="price"
+              value="asc"
+              onChange={(e) => setSort('asc')}
+            />
+            <label className="ml-3" htmlFor="asc">
+              Price (Lowest first){' '}
+            </label>
+          </div>
+          <div className="-input-item mb-3">
+            <input
+              type="radio"
+              name="desc"
+              id="price"
+              value="desc"
+              onChange={(e) => setSort('desc')}
+            />
+            <label className="ml-3" htmlFor="desc">
+              Price (Highest first){' '}
+            </label>
+          </div>
+        </div>
+      </aside>
+      <section className="-right flex-[3]">
+        <div className="right">
+          <img
+            className="catImg w-full h-[300px] mb-12 object-cover"
+            src="https://images.pexels.com/photos/1074535/pexels-photo-1074535.jpeg?auto=compress&cs=tinysrgb&w=1600"
+            alt=""
+          />
+          <List catId={catId} maxPrice={maxPrice} sort={sort} subCats="" />
+        </div>
+      </section>
+    </main>
+  )
 }
