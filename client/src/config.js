9618d4a2690f89047cc7ce13856a31f907aa7ff7
Manuel Takata <takata211011@gmail.com>
2023-02-02 21:24:36 -0500

    add env variables

---
+++ client/src/config.js
@@ -0,0 +1,4 @@
+const API_TOKEN = import.meta.env.VITE_API_TOKEN
+const API_URL = import.meta.env.VITE_API_URL
+const VITE_UPLOAD_URL = import.meta.env.VITE_UPLOAD_URL
+export { API_TOKEN, API_URL, VITE_UPLOAD_URL }
